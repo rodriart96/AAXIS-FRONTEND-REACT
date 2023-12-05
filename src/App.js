68b33fbcaba93303dcf048d48f4ab1f7c1197b04
Arturo Rodriguez <rodri.art96@hotmail.com>
2023-12-05 14:50:05 +0100

    First commit, functional basic rate exchange.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Freecurrencyapi from "@everapi/freecurrencyapi-js";
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_CURRENCY_KEY;
+  const [currencyRate, setCurrencyRate] = useState(null);
+  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const [exchangedCurrency, setExchangedCurrency] = useState("EUR");
+  const [amountToExchange, setAmountToExchange] = useState(30);
+  const [amountExchanged, setAmountExchanged] = useState(null);
+
+  const freecurrencyapi = new Freecurrencyapi(apiKey);
+
+  const getCurrencyConversion = async () => {
+    try {
+      const response = await freecurrencyapi.latest({
+        base_currency: baseCurrency,
+        currencies: exchangedCurrency,
+      });
+      setCurrencyRate(response.data[exchangedCurrency]);
+    } catch (error) {
+      console.error("Error fetching currency conversion:", error);
+    }
+  };
+
+  useEffect(() => {
+    getCurrencyConversion();
+    const  unformattedAmount = amountToExchange * currencyRate;
+    const roundedAmountExchanged = unformattedAmount.toFixed(2);
+    const formattedAmountExchanged = parseFloat(roundedAmountExchanged);
+    setAmountExchanged(formattedAmountExchanged);
+  }, [currencyRate, amountToExchange, exchangedCurrency, baseCurrency])
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p> amount to exchange:  {amountToExchange} {baseCurrency}</p>
+        <p> Exchanged Amount: {exchangedCurrency} {amountExchanged}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>${currencyRate}</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
